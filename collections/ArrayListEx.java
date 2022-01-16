e63528fa8ca683f6c5b2fa7509c7a99a6a31b2a1
johnkoo <rndkoo@gmail.com>
2022-01-16 17:22:58 +0900

    ArrayList 생성

---
+++ collections/ArrayListEx.java
@@ -0,0 +1,28 @@
+package github;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		
+
+		list.add("AAAA");
+		list.add("BBBB");
+		list.add("CCCC");
+		list.add("DDDD");
+
+		System.out.println(list);
+		
+		list.add(1,"EEEE");
+		
+		System.out.println(list);
+		
+		
+	}
+
+}
