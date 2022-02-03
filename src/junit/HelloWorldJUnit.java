c376233dd8b388439e08a8dffe0b645c84a1675c
johnkoo <rndkoo@gmail.com>
2022-02-04 00:33:25 +0900

    study junit

---
+++ src/junit/HelloWorldJUnit.java
@@ -0,0 +1,15 @@
+package junit; 
+import static org.junit.Assert.*;
+
+import org.junit.Test; 
+public class HelloWorldJUnit { 
+	@Test
+	public void test() {
+		
+		
+		
+//		assertEquals("Hello world","hello world");
+		
+		assertEquals("Hello world","Hello world"); 
+	} 
+}
---
+++ src/junit/HelloWorld_Java.java
@@ -0,0 +1,10 @@
+package junit;
+
+public class HelloWorld_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
