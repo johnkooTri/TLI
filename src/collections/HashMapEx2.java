fe0579eb954155e0d8a9c2e79cd7ca6c14318922
johnkoo <rndkoo@gmail.com>
2022-02-04 13:11:02 +0900

    getOrDefault

---
+++ src/collections/HashMapEx2.java
@@ -0,0 +1,39 @@
+package collections;
+
+import java.util.HashMap;
+
+public class HashMapEx2 { 
+	
+	public static void main(String[] args) {
+        
+    	String[] participant={"leo", "kiki", "eden"};
+    	String[] completion={"eden", "kiki"};
+    	
+    	
+    	String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        
+        System.out.println(hm.get("leo"));
+        
+        
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+            
+        
+        System.out.println(hm.get("leo"));
+        System.out.println(hm.get("leo")-1);
+        
+        
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        
+         System.out.println(answer);
+//        return answer;
+        
+    }
+}
+
+
